c390e99a36453ae4d0a4d640d6fa0867dc12ff3c
unknown <124510757+DevIsaev@users.noreply.github.com>
2026-04-08 19:15:01 +0700

    #5 Реализованы интерфейсы репозиториев

---
+++ MovieRatingsService/Domain/MovieRatingsService.Domain.Repositories.Abstractions/IReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿using MovieRatingsService.Domain.Domain.Entities;
+using MovieRatingsService.Domain.Repositories.Abstractions.Base;
+
+namespace MovieRatingsService.Domain.Repositories.Abstractions;
+
+public interface IReviewRepository : IRepository<Review, Guid>
+{
+    // Получить рецензию пользователя на конкретный фильм
+    Task<Review?> GetByUserAndMovieAsync(Guid userId, Guid movieId, CancellationToken cancellationToken);
+
+    // Все активные рецензии фильма
+    Task<IEnumerable<Review>> GetActiveByMovieAsync(Guid movieId, CancellationToken cancellationToken);
+
+    // Все рецензии пользователя
+    Task<IEnumerable<Review>> GetByUserAsync(Guid userId, CancellationToken cancellationToken);
+}
